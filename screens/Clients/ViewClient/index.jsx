9101a160b6d9ee68b1446ef6d388d1f3834b7eb7
Ahmad Habib <ahmadalkali69@gmail.com>
2022-07-04 17:15:29 +0100

    If client is deleted, all invoices in invoices collection associated with that client are deleted as well

--- screens/Clients/ViewClient/index.jsx
+++ screens/Clients/ViewClient/index.jsx
@@ -2,7 +2,14 @@ import { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { doc, deleteDoc, getDoc } from 'firebase/firestore';
+import {
+	doc,
+	deleteDoc,
+	getDoc,
+	getDocs,
+	collection,
+	writeBatch
+} from 'firebase/firestore';
 import { db } from '../../../firebase';
 import ScreenHeader from '../../../components/ScreenHeader';
 import ClientActivity from './ClientActivity';
@@ -51,14 +58,31 @@ const ViewClient = ({ navigation, route }) => {
 		return unsubscribe;
 	}, [navigation]);
 
-	const EditClient = () => {
+	const editClient = () => {
 		navigation.navigate('EditClient', { ...data });
 	};
 
-	const DeleteClient = () => {
+	const deleteClientInvoices = async (id) => {
+		const querySnapshot = await getDocs(
+			collection(db, `clients/${id}/invoices`)
+		);
+		const idArray = [];
+		querySnapshot.forEach((doc) => idArray.push(doc.id));
+
+		if (idArray.length) {
+			const batch = writeBatch(db);
+			idArray.forEach((id) => {
+				const invoiceRef = doc(db, `invoices/${id}`);
+				batch.delete(invoiceRef);
+			});
+			await batch.commit();
+		}
+	};
+
+	const deleteClient = () => {
 		Alert.alert(
 			'Confirm Delete',
-			'Are you sure you want to delete this client?',
+			'Are you sure you want to delete this client? You will also delete all invoices generated under them.',
 			[
 				{
 					text: 'Cancel'
@@ -66,6 +90,7 @@ const ViewClient = ({ navigation, route }) => {
 				{
 					text: 'OK',
 					onPress: async () => {
+						deleteClientInvoices(data.id);
 						await deleteDoc(doc(db, `clients/${data.id}`));
 						navigation.navigate('Clients');
 					}
@@ -79,11 +104,11 @@ const ViewClient = ({ navigation, route }) => {
 			<ScreenHeader heading={data.name} to="Clients" />
 
 			<View style={styles.actionsWrapper}>
-				<TouchableOpacity style={styles.action} onPress={EditClient}>
+				<TouchableOpacity style={styles.action} onPress={editClient}>
 					<AntDesign name="edit" color="#075E54" size={25} />
 					<Text>Edit</Text>
 				</TouchableOpacity>
-				<TouchableOpacity style={styles.action} onPress={DeleteClient}>
+				<TouchableOpacity style={styles.action} onPress={deleteClient}>
 					<AntDesign name="deleteuser" color="red" size={25} />
 					<Text>Delete</Text>
 				</TouchableOpacity>
