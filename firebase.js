f8db0aa626c0c7415bf8dc2106b64572d0b84af9
Ahmad Habib <ahmadalkali69@gmail.com>
2022-07-15 18:16:58 +0100

    Removed api key and fixed removed console log in view invoice

--- firebase.js
+++ firebase.js
@@ -4,9 +4,10 @@ import {
 	getAuth,
 	createUserWithEmailAndPassword,
 	signInWithEmailAndPassword,
-	signOut
+	signOut,
 } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { API_KEY } from '@env';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,13 +15,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyCD4TsT6EfT3QGSjuCSq40mPwXYkPawKwk',
+	apiKey: API_KEY,
 	authDomain: 'invoice-app-2-bf99b.firebaseapp.com',
 	projectId: 'invoice-app-2-bf99b',
 	storageBucket: 'invoice-app-2-bf99b.appspot.com',
 	messagingSenderId: '439739784677',
 	appId: '1:439739784677:web:7ecf981ca62fa8d7d8630c',
-	measurementId: 'G-8BHCZ89HCS'
+	measurementId: 'G-8BHCZ89HCS',
 };
 
 const app = initializeApp(firebaseConfig);
